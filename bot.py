cd1bfac6b610b384ba9d01c337d93cab6d383cb6
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-01-25 21:14:03 -0800

    👍

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ def is_valid_id(id_value):
 # Start komandasiga javob
 @router.message(F.text == "/start")
 async def send_welcome(message: types.Message):
-    await message.answer("Assalomu alaykum😊 \br Menga 6 ta raqamdan iborat ID yuboring.")
+    await message.answer("Assalomu alaykum 😊\n Menga 6 ta raqamdan iborat ID yuboring.")
 
 # ID qabul qilish
 @router.message(F.text)
